2e20f8cbe8c1414170488cc66d349c2e63634ce6
ruSLaN2341 <ruslan2006game@gmail.com>
2022-12-21 19:59:22 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let bd = [
+    {
+        img: "123123123",
+        title: "Obvi Apple Cider Vinegar Gummies, 60 Gummies",
+        price: "13.43$",
+        grade: 4
+    },
+    {
+        img: "123123123",
+        title: "Obvi Apple Cider Vinegar Gummies, 60 Gummies",
+        price: "13.43$",
+        grade: 4
+    },
+    {
+        img: "123123123",
+        title: "Obvi Apple Cider Vinegar Gummies, 60 Gummies",
+        price: "13.43$",
+        grade: 4
+    },
+    {
+        img: "123123123",
+        title: "Obvi Apple Cider Vinegar Gummies, 60 Gummies",
+        price: "13.43$",
+        grade: 4
+    }
+]
